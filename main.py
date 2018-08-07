75c8ab6bfe7fe84e0127f8978b5296ed6c2eb81f
yebinn <flugent.j@gmail.com>
2018-08-07 16:08:52 +0900

    슬랙 연동(할꺼야..)

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+from rtmbot import RtmBot
+from rtmbot.core import ACTIVE_PLUGINS
+
+import secret
+
+class HelloPlugin(Plugin):
+    def process_message(self, data):
+        if "tv" in data["text"]:
+            self.outputs.append([data["channel"], ""])
+
+config = {
+    "SLACK_TOKEN": secret.SLACK_TOKEN,
+    "ACTIVE_PLUGINS": ["main.HelloPlugin"]
+
+}
+bot = RtmBot(config)
+bot.start()
+
+
 print("hi you want to see tv?")
 print("sure, why not?")
 print("Let's go~!")
